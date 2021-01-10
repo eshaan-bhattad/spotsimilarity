6d699c786dae01dc3498aef3835545f4d9fdce47
josephkuang <josephjkuang@gmail.com>
2021-01-10 03:49:05 -0600

    comparison page outline

--- core/graphs.py
+++ core/graphs.py
@@ -7,8 +7,11 @@ def graph(tracks, artists, explicit, duration, popularity):
 	# Use the hovertext kw argument for hover text
 	fig = go.Figure(data=[go.Bar(x=x, y=y,)])
 	# Customize aspect
-	fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
-	                  marker_line_width=1.5, opacity=0.6)
-	fig.show()
+	fig.update_traces(marker_color='rgb(30,215,96)', marker_line_color='rgb(25,25,20)',
+	                  marker_line_width=3, opacity=1)
+	# fig.show()
+	plot_html = fig.to_html(full_html=False, include_plotlyjs=False)
+
+	return plot_html
 
 # graph(20, 14, 23, 5, 34)
